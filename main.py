ec94e8946d20a15ec591d106e1a2379bef278956
korcankaraokcu <desertricker@hotmail.com>
2016-01-23 00:32:46 +0200

    deleted unnecessary codes to speed up the program.. the greatest program ever made HAS to be fast afterall

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ class mainForm(QtWidgets.QMainWindow, mainwindow):
     def __init__(self, parent=None):
         super().__init__()
         self.setupUi(self)
-        self.window2=None
         self.pushButton.clicked.connect(self.onclick)
 
     def onclick(self):
-        if self.window2 is None:
-            self.window2 = processForm(self)
+        self.window2 = processForm(self)
         self.window2.show()
     def closeEvent(self, event):
         app = QtWidgets.QApplication.instance()
