539e4badd5ffb9ac8e79b9bedd06ad1e2cbb54b9
korcankaraokcu <desertricker@hotmail.com>
2016-01-23 22:41:24 +0200

    Moved GuiUtils to it's own py file

--- SINCE.py
+++ SINCE.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-from PyQt5.QtWidgets import QDesktopWidget,QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import QtCore
+from GuiUtils import *
 from mainwindow import Ui_MainWindow as mainwindow
 from selectprocess import Ui_MainWindow as processwindow
 
-class GuiUtils(object):
-
-#centering a window
-    def center(self):
-        self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center())
-
-#centering a child window, doesn't work as intended yet
-    def parentcenter(self):
-        self.move(self.parent().frameGeometry().center().x() - self.frameGeometry().width()/2, self.parent().frameGeometry().center().y() - self.frameGeometry().height()/2)
 
 #the mainwindow
 class mainForm(QMainWindow, mainwindow):
