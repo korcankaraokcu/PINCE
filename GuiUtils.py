df7db0e0ab7aeddae2e496c3d05abd9a0b68d4a9
korcankaraokcu <desertricker@hotmail.com>
2016-01-24 15:55:00 +0200

    fixed parentcenter() problem

--- GuiUtils.py
+++ GuiUtils.py
@@ -6,6 +6,6 @@ class GuiUtils(object):
     def center(self):
         self.move(QDesktopWidget().availableGeometry().center() - self.frameGeometry().center())
 
-#centering a child window, doesn't work as intended yet
+#centering a child window
     def parentcenter(self):
         self.move(self.parent().frameGeometry().center().x() - self.frameGeometry().width()/2, self.parent().frameGeometry().center().y() - self.frameGeometry().height()/2)
