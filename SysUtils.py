010793457888164c23bc3ef376216ef261d3e530
korcankaraokcu <desertricker@hotmail.com>
2016-01-24 02:29:11 +0200

    implemented getprocesslist() function at SysUtils.py

--- SysUtils.py
+++ SysUtils.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import psutil
+class SysUtils(object):
+#returns a list of currently working processes
+
+    def getprocesslist(self):
+        x=[]
+        y=[]
+        for p in psutil.process_iter():
+            y.append(p.as_dict(attrs=['pid','username','name']))
+            return (y)
