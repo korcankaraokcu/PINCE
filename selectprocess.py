eddbe932e40c23649c12018683aac36fb1ab8716
cagriulas <cagriulas@gmail.com>
2016-01-24 19:07:44 +0200

    updated ui
    - change mainwindow title
    - reorganize gui layouts

--- selectprocess.py
+++ selectprocess.py
@@ -2,8 +2,7 @@
 
 # Form implementation generated from reading ui file 'selectprocess.ui'
 #
-# Created: Sun Jan 24 16:54:05 2016
-#      by: PyQt5 UI code generator 5.2.1
+# Created by: PyQt5 UI code generator 5.5.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -13,25 +12,26 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(QtCore.Qt.WindowModal)
-        MainWindow.resize(597, 340)
+        MainWindow.resize(443, 420)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.formLayout = QtWidgets.QFormLayout(self.centralwidget)
-        self.formLayout.setObjectName("formLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setObjectName("label")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.horizontalLayout.addWidget(self.label)
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setObjectName("lineEdit")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lineEdit)
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.verticalLayout.addLayout(self.horizontalLayout)
         self.processtable = QtWidgets.QTableWidget(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.processtable.sizePolicy().hasHeightForWidth())
         self.processtable.setSizePolicy(sizePolicy)
-        self.processtable.setMinimumSize(QtCore.QSize(579, 292))
-        self.processtable.setMaximumSize(QtCore.QSize(579, 292))
         self.processtable.setAutoFillBackground(False)
         self.processtable.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
         self.processtable.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
@@ -51,7 +51,19 @@ class Ui_MainWindow(object):
         self.processtable.horizontalHeader().setStretchLastSection(True)
         self.processtable.verticalHeader().setVisible(False)
         self.processtable.verticalHeader().setStretchLastSection(False)
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.SpanningRole, self.processtable)
+        self.verticalLayout.addWidget(self.processtable)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.pushButton_Open = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_Open.setObjectName("pushButton_Open")
+        self.horizontalLayout_2.addWidget(self.pushButton_Open)
+        self.pushButton_Close = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_Close.setObjectName("pushButton_Close")
+        self.horizontalLayout_2.addWidget(self.pushButton_Close)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.pushButton_CreateProcess = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_CreateProcess.setObjectName("pushButton_CreateProcess")
+        self.verticalLayout.addWidget(self.pushButton_CreateProcess)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -68,4 +80,7 @@ class Ui_MainWindow(object):
         item.setText(_translate("MainWindow", "Username"))
         item = self.processtable.horizontalHeaderItem(2)
         item.setText(_translate("MainWindow", "Process Name"))
+        self.pushButton_Open.setText(_translate("MainWindow", "Open"))
+        self.pushButton_Close.setText(_translate("MainWindow", "Cancel"))
+        self.pushButton_CreateProcess.setText(_translate("MainWindow", "Create Process"))
 
