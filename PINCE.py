09a6718e0c1da2c7ed07f230e776ec0a7d838d5f
korcankaraokcu <desertricker@hotmail.com>
2016-03-01 18:35:30 +0200

    Reformatted the ENTIRE code PINCE is now streamlined for PEP 8 Deleted classes from the Utility and GDB_ENGINE files renamed most of the variables and function names, they are now more readable Changed most of the parameter syntagma to param=type from only param

--- PINCE.py
+++ PINCE.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication,QMainWindow,QTableWidgetItem,QMessageBox,QDialog
-from PyQt5.QtCore import Qt,QThread
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox, QDialog
+from PyQt5.QtCore import Qt, QThread
 from GuiUtils import *
 from SysUtils import *
-from GDB_Engine import GDB_Engine
-from GUI.mainwindow import Ui_MainWindow as mainwindow
-from GUI.selectprocess import Ui_MainWindow as processwindow
-from GUI.addaddressmanuallydialog import Ui_Dialog as manualaddressdialog
+from GDB_Engine import *
+from GUI.mainwindow import Ui_MainWindow as MainWindow
+from GUI.selectprocess import Ui_MainWindow as ProcessWindow
+from GUI.addaddressmanuallydialog import Ui_Dialog as ManualAddressDialog
 from threading import Thread
 
-#the PID of the process we'll attach to
-currentpid=0
+# the PID of the process we'll attach to
+currentpid = 0
 
-#test
+
+# test
 class WorkerThread(QThread):
     def run(self):
-        GDB_Engine.test()
+        test()
+
 
 class WorkerThread2(QThread):
     def run(self):
-        GDB_Engine.test2()
+        test2()
 
-#the mainwindow
-class mainForm(QMainWindow, mainwindow):
+
+# the mainwindow
+class MainForm(QMainWindow, MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        GuiUtils.center(self)
+        center(self)
         self.processbutton.clicked.connect(self.processbutton_onclick)
-        self.pushButton_NewFirstScan.clicked.connect(self.NewFirstScan_onclick)
-        self.pushButton_NextScan.clicked.connect(self.NextScan_onclick)
-        self.pushButton_AddAddressManually.clicked.connect(self.AddAddressManually_onclick)
+        self.pushButton_NewFirstScan.clicked.connect(self.newfirstscan_onclick)
+        self.pushButton_NextScan.clicked.connect(self.nextscan_onclick)
+        self.pushButton_AddAddressManually.clicked.connect(self.addaddressmanually_onclick)
         self.processbutton.setIcon(QIcon.fromTheme('computer'))
         self.pushButton_Open.setIcon(QIcon.fromTheme('document-open'))
         self.pushButton_Save.setIcon(QIcon.fromTheme('document-save'))
@@ -39,73 +42,74 @@ class mainForm(QMainWindow, mainwindow):
         self.pushButton_CopyToAddressList.setIcon(QIcon.fromTheme('emblem-downloads'))
         self.pushButton_CleanAddressList.setIcon(QIcon.fromTheme('user-trash'))
 
-    def AddAddressManually_onclick(self):
-        self.manualaddressdialog=ManualAddressDialog()
-        self.manualaddressdialog.exec_()
+    def addaddressmanually_onclick(self):
+        self.manual_address_dialog = ManualAddressDialogForm()
+        self.manual_address_dialog.exec_()
 
-    def NewFirstScan_onclick(self):
-        if self.pushButton_NewFirstScan.text()=="First Scan":
+    def newfirstscan_onclick(self):
+        if self.pushButton_NewFirstScan.text() == "First Scan":
             self.pushButton_NextScan.setEnabled(True)
             self.pushButton_UndoScan.setEnabled(True)
             self.pushButton_NewFirstScan.setText("New Scan")
             return
-        if self.pushButton_NewFirstScan.text()=="New Scan":
+        if self.pushButton_NewFirstScan.text() == "New Scan":
             self.pushButton_NextScan.setEnabled(False)
             self.pushButton_UndoScan.setEnabled(False)
             self.pushButton_NewFirstScan.setText("First Scan")
 
-    def NextScan_onclick(self):
-        #thread1=WorkerThread()                                #thread test
-        #thread1.run()
-        #thread2=WorkerThread2()
-        #thread2.run()
-        #t=Thread(target=GDB_Engine.test)
-        #t2=Thread(target=GDB_Engine.test2)
-        #t.start()
-        #t2.start()
-        if self.tableWidget_valuesearchtable.rowCount()<=0:
+    def nextscan_onclick(self):
+        # thread1=WorkerThread()                                #thread test
+        # thread1.run()
+        # thread2=WorkerThread2()
+        # thread2.run()
+        # t=Thread(target=test)
+        # t2=Thread(target=test2)
+        # t.start()
+        # t2.start()
+        if self.tableWidget_valuesearchtable.rowCount() <= 0:
             return
 
-#shows the process select window
+    # shows the process select window
     def processbutton_onclick(self):
-        self.processwindow = processForm(self)
+        self.processwindow = ProcessForm(self)
         self.processwindow.show()
 
-#closes all windows on exit
+    # closes all windows on exit
     def closeEvent(self, event):
-        if not currentpid==0:
-            GDB_Engine.deattach()
-        app = QApplication.instance()
-        app.closeAllWindows()
+        if not currentpid == 0:
+            detach()
+        application = QApplication.instance()
+        application.closeAllWindows()
 
-#process select window
-class processForm(QMainWindow, processwindow):
+
+# process select window
+class ProcessForm(QMainWindow, ProcessWindow):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
-        GuiUtils.parentcenter(self)
-        processlist=SysUtils.getprocesslist()
-        self.refreshprocesstable(self.processtable, processlist)
-        self.pushButton_Close.clicked.connect(self.pushButton_Close_onclick)
-        self.pushButton_Open.clicked.connect(self.pushButton_Open_onclick)
-        self.lineEdit_searchprocess.textChanged.connect(self.generatenewlist)
-        self.processtable.itemDoubleClicked.connect(self.pushButton_Open_onclick)
+        parentcenter(self)
+        processlist = get_process_list()
+        self.refresh_process_table(self.processtable, processlist)
+        self.pushButton_Close.clicked.connect(self.pushbutton_close_onclick)
+        self.pushButton_Open.clicked.connect(self.pushbutton_open_onclick)
+        self.lineEdit_searchprocess.textChanged.connect(self.generate_new_list)
+        self.processtable.itemDoubleClicked.connect(self.pushbutton_open_onclick)
 
-    def generatenewlist(self):
+    def generate_new_list(self):
         if self.lineEdit_searchprocess.isModified():
-            text=self.lineEdit_searchprocess.text()
-            processlist=SysUtils.searchinprocessesByName(text)
-            self.refreshprocesstable(self.processtable,processlist)
+            text = self.lineEdit_searchprocess.text()
+            processlist = search_in_processes_by_name(text)
+            self.refresh_process_table(self.processtable, processlist)
         else:
             return
 
-#closes the window whenever ESC key is pressed
+    # closes the window whenever ESC key is pressed
     def keyPressEvent(self, e):
-        if e.key()==Qt.Key_Escape:
+        if e.key() == Qt.Key_Escape:
             self.close()
 
-#lists currently working processes to table
-    def refreshprocesstable(self, tablewidget, processlist):
+    # lists currently working processes to table
+    def refresh_process_table(self, tablewidget, processlist):
         tablewidget.setRowCount(0)
         tablewidget.setRowCount(len(processlist))
         for i, row in enumerate(processlist):
@@ -113,60 +117,64 @@ class processForm(QMainWindow, processwindow):
             tablewidget.setItem(i, 1, QTableWidgetItem(row.username()))
             tablewidget.setItem(i, 2, QTableWidgetItem(row.name()))
 
-#self-explanatory
-    def pushButton_Close_onclick(self):
+    # self-explanatory
+    def pushbutton_close_onclick(self):
         self.close()
 
-#gets the pid out of the selection to set currentpid
-    def pushButton_Open_onclick(self):
+    # gets the pid out of the selection to set currentpid
+    def pushbutton_open_onclick(self):
         global currentpid
-        curItem = self.processtable.item(self.processtable.currentIndex().row(),0)
-        if curItem is None:
-            QMessageBox.information(self, "Error","Please select a process first")
+        currentitem = self.processtable.item(self.processtable.currentIndex().row(), 0)
+        if currentitem is None:
+            QMessageBox.information(self, "Error", "Please select a process first")
         else:
-            pid=int(curItem.text())
-            if not SysUtils.isprocessvalid(pid):
-                QMessageBox.information(self, "Error","Selected process is not valid")
+            pid = int(currentitem.text())
+            if not is_process_valid(pid):
+                QMessageBox.information(self, "Error", "Selected process is not valid")
                 return
-            if pid==currentpid:
-                QMessageBox.information(self, "Error","You're debugging this process already")
+            if pid == currentpid:
+                QMessageBox.information(self, "Error", "You're debugging this process already")
                 return
-            tracedby=SysUtils.isTraced(pid)
+            tracedby = is_traced(pid)
             if tracedby:
-                QMessageBox.information(self, "Error","That process is already being traced by " + tracedby + ", could not attach to the process")
+                QMessageBox.information(self, "Error",
+                                        "That process is already being traced by " + tracedby + ", could not attach to the process")
                 return
-            print("processing")                                                      #progressbar koy buraya
-            result=GDB_Engine.canattach(str(pid))
+            print("processing")  # progressbar koy buraya
+            result = can_attach(str(pid))
             if not result:
-                print("done")                                                        #progressbar finish
-                QMessageBox.information(self, "Error","Permission denied, could not attach to the process")
+                print("done")  # progressbar finish
+                QMessageBox.information(self, "Error", "Permission denied, could not attach to the process")
                 return
-            if not currentpid==0:
-                GDB_Engine.deattach()
-            currentpid=pid
-            IsThreadInjectionSuccessful=GDB_Engine.attach(str(currentpid))
-            p=SysUtils.getprocessinformation(currentpid)
+            if not currentpid == 0:
+                detach()
+            currentpid = pid
+            is_thread_injection_successful = attach(str(currentpid))
+            p = get_process_information(currentpid)
             self.parent().label_SelectedProcess.setText(str(p.pid) + " - " + p.name())
             self.parent().QWidget_Toolbox.setEnabled(True)
             self.parent().pushButton_NextScan.setEnabled(False)
             self.parent().pushButton_UndoScan.setEnabled(False)
-            readable_only,writeable,executable,readable=SysUtils.getmemoryregionsByPerms(currentpid)              #test
-            SysUtils.excludeSystemMemoryRegions(readable)
+            readable_only, writeable, executable, readable = get_memory_regions_by_perms(currentpid)  # test
+            exclude_system_memory_regions(readable)
             print(len(readable))
-            print("done")                                                                 #progressbar finish
-            if not IsThreadInjectionSuccessful:
-                QMessageBox.information(self, "Warning","Unable to inject thread, PINCE may(will) not work properly")
+            print("done")  # progressbar finish
+            if not is_thread_injection_successful:
+                QMessageBox.information(self, "Warning", "Unable to inject thread, PINCE may(will) not work properly")
             self.close()
 
-#Add Address Manually Dialog
-class ManualAddressDialog(QDialog,manualaddressdialog):
-    def __init__(self,parent=None):
+
+# Add Address Manually Dialog
+class ManualAddressDialogForm(QDialog, ManualAddressDialog):
+    def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.setupUi(self)
 
+
 if __name__ == "__main__":
     import sys
+
     app = QApplication(sys.argv)
-    window = mainForm()
+    window = MainForm()
     window.show()
     sys.exit(app.exec_())
