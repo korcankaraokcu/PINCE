f080d24d3fcad59a09b39bc83d2849d88e96538a
korcankaraokcu <desertricker@hotmail.com>
2016-03-07 22:30:12 +0200

    Added comments next to the regular expressions to clarify them This will help future developers since gdb/mi's output is somewhat weird

--- GDB_Engine.py
+++ GDB_Engine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from re import search, split, match
-from threading import Lock, Thread
+from threading import Lock
 from time import sleep
 import pexpect
 
@@ -8,8 +8,10 @@ import SysUtils
 
 child = object  # this object will be used with pexpect operations
 infinite_thread_location = str  # location of the injected thread that runs forever at background
-infinite_thread_id=str  # id of the injected thread that runs forever at background
+infinite_thread_id = str  # id of the injected thread that runs forever at background
 lock = Lock()
+
+#The comments next to the regular expressions shows the expected gdb output, an elucidating light for the future developers
 
 
 # issues the command sent, str is command string
@@ -29,7 +31,7 @@ def can_attach(pid=str):
     a.expect_exact("(gdb) ")
 
     # return true if attaching is successful, false if not, then quit
-    if search(r"Operation not permitted", a.before):
+    if search(r"Operation not permitted", a.before):  #literal string
         a.sendline("q")
         a.close()
         return False
@@ -63,6 +65,7 @@ def detach():
     child.sendline("q")
     child.close()
 
+
 # Injects a thread that runs forever at the background, it'll be used to execute GDB commands on
 # Also saves the injected thread's location and ID as strings, then switches to that thread and stops it
 def inject_additional_threads():
@@ -74,15 +77,15 @@ def inject_additional_threads():
     send_command("call dlopen(" + injectionpath + ", 2)")
     result = split("call injection", send_command("call injection()"))
     send_command("c &")
-    threadaddress = search(r"0x\w*", result[1])
+    threadaddress = search(r"0x\w*", result[1]) #$ = 0xDEADBEEF
 
     # Return True is injection is successful, False if not
     if not threadaddress:
         return False
-    match_from_info_threads=search(r"\d+.*" + threadaddress.group(0), send_command("info threads")).group(0)
-    infinite_thread_id=split(" ",match_from_info_threads)[0]
+    match_from_info_threads = search(r"\d+.*" + threadaddress.group(0), send_command("info threads")).group(0)  #1 Thread 0xDEADBEEF
+    infinite_thread_id = split(" ", match_from_info_threads)[0]
     infinite_thread_location = threadaddress
-    send_command("thread "+infinite_thread_id)
+    send_command("thread " + infinite_thread_id)
     send_command("interrupt")
     return True
 
@@ -95,22 +98,26 @@ def await_inferior_exit():
             print("kek")
             break
 
-#return the value of the address if the address is valid, return the string "??" if not
-def read_single_address(address=str):
-    result=send_command("print *" + address)
-    filteredresult=search(r"\$\d+\s*=\s*\d+",result)
+
+# return the value of the address if the address is valid, return the string "??" if not
+def read_single_address(address=str,typeofaddress=str):
+    valuetype=typeofaddress
+    result = send_command("print *" + address)
+    filteredresult = search(r"\$\d+\s*=\s*\d+", result)  #$ = 16123512
     if filteredresult:
-        return split(" ",filteredresult.group(0))[-1]
+        return split(" ", filteredresult.group(0))[-1]
     return "??"
+
 
 def test():
     for x in range(0, 10):
-        print(send_command("find 0x00400000,+500,1"))
+        print(send_command("x/b 0x00400000"))
 
 
 def test2():
     for x in range(0, 10):
         print(send_command("disas /r 0x00400000,+10"))
+
 
 def test3():
     global child
