0bbdd7d90aa628923303d5d21ae5fd795c62bd5d
korcankaraokcu <desertricker@hotmail.com>
2016-03-09 19:29:36 +0200

    AddAddressManually dialog now takes the intended value type into account

--- GuiUtils.py
+++ GuiUtils.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 from PyQt5.QtWidgets import QDesktopWidget
 
-combobox_value_dict={             #test
-    0:"byte",
-    1:"short",
-    2:"int",
-    3:"long",
-    4:"float",
-    5:"double",
-
+# A dictionary used to convert value_combobox index to gdb/mi command
+combobox_value_dict = {
+    0: "db",  # byte
+    1: "dh",  # 2bytes
+    2: "dw",  # 4bytes
+    3: "dg",  # 8bytes
+    4: "fw",  # float
+    5: "fg",  # double
+    6: "s",  # string
+    7: "xb"  # array of bytes
 }
+
 
 # centering a window
 def center(window):
@@ -20,5 +23,8 @@ def center(window):
 def center_parent(window):
     window.move(window.parent().frameGeometry().center() - window.frameGeometry().center())
 
-def valuecombobox_to_valuetype(combobox):            #test
-    print("lasf")
+
+# return a string corresponding to the selected index
+# returns "out of bounds" string if the index doesn't match the dictionary
+def valuecombobox_to_valuetype(index=int):
+    return combobox_value_dict.get(index, "out of bounds")
