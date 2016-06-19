4fcf75a02de8cc4fb9e347f7c98693275e84785f
korcankaraokcu <korcankaraokcu@gmail.com>
2016-06-19 03:02:04 +0300

    Simplified the example.c

--- Injection/example.c
+++ Injection/example.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 
 void *infinite_thread(void *a);
 
@@ -15,21 +13,8 @@ void inject_infinite_thread()
 
 void *infinite_thread(void *a)
 {
-	int milliseconds=500;
-	struct timespec ts;
-	ts.tv_sec = milliseconds / 1000;
-	ts.tv_nsec = (milliseconds % 1000) * 1000000;
-	int selfpid=getpid();
-	char abort_file[100];
-	sprintf(abort_file, "/tmp/PINCE-connection/%d/abort.txt", selfpid);
-	FILE *exit;
 	while(1)
 	{
-		nanosleep(&ts, NULL);
-		exit = fopen(abort_file, "r");
-		if(exit!=NULL){
-			fclose(exit);
-			return 0;
-		}
+		sleep(1);
 	}
 }
