b6f7383b9a71395fc66fbd0520f871d2f7dcdca1
korcankaraokcu <korcankaraokcu@gmail.com>
2016-07-26 23:52:29 +0300

    Generalized and optimized the file communication system

--- gdb_python_scripts/GDBCommandExtensions.py
+++ gdb_python_scripts/GDBCommandExtensions.py
@@ -9,6 +9,7 @@ import struct
 gdbvalue = gdb.parse_and_eval("$PINCE_PATH")
 PINCE_PATH = gdbvalue.string()
 sys.path.append(PINCE_PATH)  # Adds the PINCE directory to PYTHONPATH to import libraries from PINCE
+
 import gdb_python_scripts.ScriptUtils as ScriptUtils
 import SysUtils
 import type_defs
@@ -30,21 +31,34 @@ INDEX_AOB = type_defs.VALUE_INDEX.INDEX_AOB
 ARCH_32 = type_defs.INFERIOR_ARCH.ARCH_32
 ARCH_64 = type_defs.INFERIOR_ARCH.ARCH_64
 
+inferior = gdb.selected_inferior()
+pid = inferior.pid
+recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
+send_file = SysUtils.get_ipc_to_PINCE_file(pid)
+if str(gdb.parse_and_eval("$rax")) == "void":
+    current_arch = ARCH_32
+else:
+    current_arch = ARCH_64
+
+
+def receive_from_pince():
+    return pickle.load(open(recv_file, "rb"))
+
+
+def send_to_pince(contents_send):
+    pickle.dump(contents_send, open(send_file, "wb"))
+
 
 class ReadMultipleAddresses(gdb.Command):
     def __init__(self):
         super(ReadMultipleAddresses, self).__init__("pince-read-multiple-addresses", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = []
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        file_contents_recv = pickle.load(open(recv_file, "rb"))
+        contents_send = []
+        contents_recv = receive_from_pince()
 
-        # file_contents_recv format: [[address1, index1, length1, unicode1, zero_terminate1],[address2, ...], ...]
-        for item in file_contents_recv:
+        # contents_recv format: [[address1, index1, length1, unicode1, zero_terminate1],[address2, ...], ...]
+        for item in contents_recv:
             address = item[0]
             index = item[1]
             try:
@@ -60,8 +74,8 @@ class ReadMultipleAddresses(gdb.Command):
             except IndexError:
                 zero_terminate = True
             readed = ScriptUtils.read_single_address(address, index, length, unicode, zero_terminate)
-            file_contents_send.append(readed)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send.append(readed)
+        send_to_pince(contents_send)
 
 
 class SetMultipleAddresses(gdb.Command):
@@ -69,16 +83,13 @@ class SetMultipleAddresses(gdb.Command):
         super(SetMultipleAddresses, self).__init__("pince-set-multiple-addresses", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
-        file_contents_recv = pickle.load(open(recv_file, "rb"))
+        contents_recv = receive_from_pince()
 
-        # last item of file_contents_recv is always value, so we pop it from the list first
-        value = file_contents_recv.pop()
+        # last item of contents_recv is always value, so we pop it from the list first
+        value = contents_recv.pop()
 
-        # file_contents_recv format after popping the value: [[address1, index1],[address2, index2], ...]
-        for item in file_contents_recv:
+        # contents_recv format after popping the value: [[address1, index1],[address2, index2], ...]
+        for item in contents_recv:
             address = item[0]
             index = item[1]
             ScriptUtils.set_single_address(address, index, value)
@@ -89,18 +100,14 @@ class ReadSingleAddress(gdb.Command):
         super(ReadSingleAddress, self).__init__("pince-read-single-address", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
-        file_contents_recv = pickle.load(open(recv_file, "rb"))
-        address = file_contents_recv[0]
-        value_index = file_contents_recv[1]
-        length = file_contents_recv[2]
-        is_unicode = file_contents_recv[3]
-        zero_terminate = file_contents_recv[4]
-        file_contents_send = ScriptUtils.read_single_address(address, value_index, length, is_unicode, zero_terminate)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+        contents_recv = receive_from_pince()
+        address = contents_recv[0]
+        value_index = contents_recv[1]
+        length = contents_recv[2]
+        is_unicode = contents_recv[3]
+        zero_terminate = contents_recv[4]
+        contents_send = ScriptUtils.read_single_address(address, value_index, length, is_unicode, zero_terminate)
+        send_to_pince(contents_send)
 
 
 class IgnoreErrors(gdb.Command):
@@ -119,15 +126,12 @@ class CLIOutput(gdb.Command):
         super(CLIOutput, self).__init__("cli-output", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
         try:
-            file_contents_send = gdb.execute(arg, from_tty, to_string=True)
+            contents_send = gdb.execute(arg, from_tty, to_string=True)
         except Exception as e:
-            file_contents_send = str(e)
-        print(file_contents_send)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send = str(e)
+        print(contents_send)
+        send_to_pince(contents_send)
 
 
 class ParseConvenienceVariables(gdb.Command):
@@ -135,20 +139,16 @@ class ParseConvenienceVariables(gdb.Command):
         super(ParseConvenienceVariables, self).__init__("pince-parse-convenience-variables", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = []
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        file_contents_recv = pickle.load(open(recv_file, "rb"))
-        for item in file_contents_recv:
+        contents_send = []
+        contents_recv = receive_from_pince()
+        for item in contents_recv:
             try:
                 value = gdb.parse_and_eval(item)
                 parsed_value = str(value)
             except:
                 parsed_value = None
-            file_contents_send.append(parsed_value)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send.append(parsed_value)
+        send_to_pince(contents_send)
 
 
 class ReadRegisters(gdb.Command):
@@ -156,15 +156,8 @@ class ReadRegisters(gdb.Command):
         super(ReadRegisters, self).__init__("pince-read-registers", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = {"cf": "0", "pf": "0", "af": "0", "zf": "0", "sf": "0", "tf": "0", "if": "0", "df": "0",
-                              "of": "0"}
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        if str(gdb.parse_and_eval("$rax")) == "void":
-            current_arch = ARCH_32
-        else:
-            current_arch = ARCH_64
+        contents_send = {"cf": "0", "pf": "0", "af": "0", "zf": "0", "sf": "0", "tf": "0", "if": "0", "df": "0",
+                         "of": "0"}
         if current_arch == ARCH_64:
             general_register_list = REGISTERS_64
         else:
@@ -173,27 +166,27 @@ class ReadRegisters(gdb.Command):
         for item in general_register_list:
             result = gdb.execute("p/x $" + item, from_tty, to_string=True)
             parsed_result = regex_hex.search(result).group(0)
-            file_contents_send[item] = parsed_result
+            contents_send[item] = parsed_result
         result = gdb.execute("p/t $eflags", from_tty, to_string=True)
         parsed_result = re.search(r"=\s+\d+", result).group(0).split()[-1]  # $8 = 1010010011
         reversed_parsed_result = "".join(reversed(parsed_result))
         try:
-            file_contents_send["cf"] = reversed_parsed_result[0]
-            file_contents_send["pf"] = reversed_parsed_result[2]
-            file_contents_send["af"] = reversed_parsed_result[4]
-            file_contents_send["zf"] = reversed_parsed_result[6]
-            file_contents_send["sf"] = reversed_parsed_result[7]
-            file_contents_send["tf"] = reversed_parsed_result[8]
-            file_contents_send["if"] = reversed_parsed_result[9]
-            file_contents_send["df"] = reversed_parsed_result[10]
-            file_contents_send["of"] = reversed_parsed_result[11]
+            contents_send["cf"] = reversed_parsed_result[0]
+            contents_send["pf"] = reversed_parsed_result[2]
+            contents_send["af"] = reversed_parsed_result[4]
+            contents_send["zf"] = reversed_parsed_result[6]
+            contents_send["sf"] = reversed_parsed_result[7]
+            contents_send["tf"] = reversed_parsed_result[8]
+            contents_send["if"] = reversed_parsed_result[9]
+            contents_send["df"] = reversed_parsed_result[10]
+            contents_send["of"] = reversed_parsed_result[11]
         except IndexError:
             pass
         for item in REGISTERS_SEGMENT:
             result = gdb.execute("p/x $" + item, from_tty, to_string=True)
             parsed_result = regex_hex.search(result).group(0)
-            file_contents_send[item] = parsed_result
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send[item] = parsed_result
+        send_to_pince(contents_send)
 
 
 class ReadFloatRegisters(gdb.Command):
@@ -201,23 +194,20 @@ class ReadFloatRegisters(gdb.Command):
         super(ReadFloatRegisters, self).__init__("pince-read-float-registers", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = {}
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
+        contents_send = {}
 
         # st0-7
         for index in range(8):
             current_register = "st" + str(index)
             value = gdb.parse_and_eval("$" + current_register)
-            file_contents_send[current_register] = str(value)
+            contents_send[current_register] = str(value)
 
         # xmm0-7
         for index in range(8):
             current_register = "xmm" + str(index)
             value = gdb.parse_and_eval("$" + current_register + ".uint128")
-            file_contents_send[current_register] = str(value)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send[current_register] = str(value)
+        send_to_pince(contents_send)
 
 
 class GetStackTraceInfo(gdb.Command):
@@ -225,14 +215,7 @@ class GetStackTraceInfo(gdb.Command):
         super(GetStackTraceInfo, self).__init__("pince-get-stack-trace-info", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = []
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        if str(gdb.parse_and_eval("$rax")) == "void":
-            current_arch = ARCH_32
-        else:
-            current_arch = ARCH_64
+        contents_send = []
         if current_arch == ARCH_64:
             sp_register = "rsp"
             result = gdb.execute("p/x $rsp", from_tty, to_string=True)
@@ -264,8 +247,8 @@ class GetStackTraceInfo(gdb.Command):
                 return_address_with_info = re.search(r"0x[0-9a-fA-F]+.*:", result).group(0).split(":")[0]
             else:
                 return_address_with_info = "<unavailable>"
-            file_contents_send.append([return_address_with_info, frame_address_with_difference])
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send.append([return_address_with_info, frame_address_with_difference])
+        send_to_pince(contents_send)
 
 
 class GetStackInfo(gdb.Command):
@@ -273,20 +256,17 @@ class GetStackInfo(gdb.Command):
         super(GetStackInfo, self).__init__("pince-get-stack-info", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = []
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        if str(gdb.parse_and_eval("$rax")) == "void":
-            chunk_size = 4
-            float_format = "f"
-            stack_register = "esp"
-            result = gdb.execute("p/x $esp", from_tty, to_string=True)
-        else:
+        contents_send = []
+        if current_arch == ARCH_64:
             chunk_size = 8
             float_format = "d"
             stack_register = "rsp"
             result = gdb.execute("p/x $rsp", from_tty, to_string=True)
+        else:
+            chunk_size = 4
+            float_format = "f"
+            stack_register = "esp"
+            result = gdb.execute("p/x $esp", from_tty, to_string=True)
         stack_address = int(re.search(r"0x[0-9a-fA-F]+", result).group(0), 16)  # $6 = 0x7f0bc0b6bb40
         mem_file = "/proc/" + str(pid) + "/mem"
         with open(mem_file, "rb") as FILE:
@@ -303,8 +283,8 @@ class GetStackInfo(gdb.Command):
                 hex_data = "0x" + "".join(format(n, '02x') for n in reversed(read))
                 int_data = str(int(hex_data, 16))
                 float_data = str(struct.unpack_from(float_format, read)[0])
-                file_contents_send.append([stack_indicator, hex_data, int_data, float_data])
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+                contents_send.append([stack_indicator, hex_data, int_data, float_data])
+        send_to_pince(contents_send)
 
 
 class GetFrameReturnAddresses(gdb.Command):
@@ -312,10 +292,7 @@ class GetFrameReturnAddresses(gdb.Command):
         super(GetFrameReturnAddresses, self).__init__("pince-get-frame-return-addresses", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        file_contents_send = []
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
+        contents_send = []
         result = gdb.execute("bt", from_tty, to_string=True)
 
         # Example: #10 0x000000000040c45a in--->#10--->10
@@ -335,8 +312,8 @@ class GetFrameReturnAddresses(gdb.Command):
                 return_address_with_info = re.search(r"0x[0-9a-fA-F]+.*:", result).group(0).split(":")[0]
             else:
                 return_address_with_info = "<unavailable>"
-            file_contents_send.append(return_address_with_info)
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            contents_send.append(return_address_with_info)
+        send_to_pince(contents_send)
 
 
 class GetFrameInfo(gdb.Command):
@@ -344,21 +321,17 @@ class GetFrameInfo(gdb.Command):
         super(GetFrameInfo, self).__init__("pince-get-frame-info", gdb.COMMAND_USER)
 
     def invoke(self, arg, from_tty):
-        inferior = gdb.selected_inferior()
-        pid = inferior.pid
-        send_file = SysUtils.get_ipc_to_PINCE_file(pid)
-        recv_file = SysUtils.get_ipc_from_PINCE_file(pid)
-        file_contents_recv = pickle.load(open(recv_file, "rb"))
+        contents_recv = receive_from_pince()
         result = gdb.execute("bt", from_tty, to_string=True)
 
         # Example: #10 0x000000000040c45a in--->#10--->10
         max_frame = re.findall(r"#\d+\s+0x[0-9a-fA-F]+\s+in", result)[-1].split()[0].replace("#", "")
-        if 0 <= int(file_contents_recv) <= int(max_frame):
-            file_contents_send = gdb.execute("info frame " + file_contents_recv, from_tty, to_string=True)
+        if 0 <= int(contents_recv) <= int(max_frame):
+            contents_send = gdb.execute("info frame " + contents_recv, from_tty, to_string=True)
         else:
-            print("Frame " + file_contents_recv + " doesn't exist")
-            file_contents_send = None
-        pickle.dump(file_contents_send, open(send_file, "wb"))
+            print("Frame " + contents_recv + " doesn't exist")
+            contents_send = None
+        send_to_pince(contents_send)
 
 
 IgnoreErrors()
