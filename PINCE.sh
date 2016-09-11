0a6a3580a734c31eb3e6faef289f43fa6231c798
korcankaraokcu <korcankaraokcu@gmail.com>
2016-09-12 01:55:12 +0300

    Added temporary launch and install scripts

---
+++ PINCE.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+: '
+Copyright (C) 2016 Korcan Karaokçu <korcankaraokcu@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'
+
+# Change this bullcrap when polkit is implemented
+OS=$(lsb_release -si)
+if [ $OS = "Debian" ]; then
+  gksudo python3 PINCE.py
+else
+  sudo python3 PINCE.py
+fi
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+: '
+Copyright (C) 2016 Korcan Karaokçu <korcankaraokcu@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-pip
+sudo apt-get install python3-pyqt5
+sudo pip3 install psutil
+sudo pip3 install pexpect
+mkdir -p gdb_pince
+cd gdb_pince
+
+# clean the directory if another install happened
+rm -rf gdb-7.11.1
+
+wget "http://ftp.gnu.org/gnu/gdb/gdb-7.11.1.tar.gz"
+tar -zxvf gdb-7.11.1.tar.gz
+cd gdb-7.11.1
+
+# Dependencies required for compiling gdb
+sudo apt-get install python3-dev
+sudo apt-get install gcc
+
+# FIXME: Is setting prefix as "/usr" bad?
+CC=gcc ./configure --prefix=/usr --with-python=python3 && make && sudo make -C gdb install
+
+# Relocating PINCE files
+cd
+auto_load_command="set auto-load safe-path /"
+echo "\n"$auto_load_command >> .gdbinit
+echo "PINCE has been installed successfully!"
+echo "Now, just run 
