f98db88d3edc570e180891e4f4ab03a9452e8b46
korcankaraokcu <korcankaraokcu@gmail.com>
2016-12-09 14:20:11 +0200

    Upgraded FunctionsInfoWidget

--- GUI/FunctionsInfoWidget.py
+++ GUI/FunctionsInfoWidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'FunctionsInfoWidget.ui'
 #
-# Created: Fri Dec  2 21:30:58 2016
+# Created: Fri Dec  9 14:18:45 2016
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,25 +15,6 @@ class Ui_Form(object):
         Form.resize(640, 555)
         self.gridLayout = QtWidgets.QGridLayout(Form)
         self.gridLayout.setObjectName("gridLayout")
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.lineEdit_SearchInput = QtWidgets.QLineEdit(Form)
-        self.lineEdit_SearchInput.setObjectName("lineEdit_SearchInput")
-        self.horizontalLayout.addWidget(self.lineEdit_SearchInput)
-        self.pushButton_Search = QtWidgets.QPushButton(Form)
-        self.pushButton_Search.setObjectName("pushButton_Search")
-        self.horizontalLayout.addWidget(self.pushButton_Search)
-        self.pushButton_Help = QtWidgets.QPushButton(Form)
-        self.pushButton_Help.setText("")
-        self.pushButton_Help.setObjectName("pushButton_Help")
-        self.horizontalLayout.addWidget(self.pushButton_Help)
-        self.verticalLayout.addLayout(self.horizontalLayout)
-        self.lineEdit_AddressInfo = QtWidgets.QLineEdit(Form)
-        self.lineEdit_AddressInfo.setReadOnly(True)
-        self.lineEdit_AddressInfo.setObjectName("lineEdit_AddressInfo")
-        self.verticalLayout.addWidget(self.lineEdit_AddressInfo)
         self.tableWidget_SymbolInfo = QtWidgets.QTableWidget(Form)
         self.tableWidget_SymbolInfo.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tableWidget_SymbolInfo.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
@@ -49,8 +30,23 @@ class Ui_Form(object):
         self.tableWidget_SymbolInfo.verticalHeader().setVisible(False)
         self.tableWidget_SymbolInfo.verticalHeader().setDefaultSectionSize(20)
         self.tableWidget_SymbolInfo.verticalHeader().setMinimumSectionSize(20)
-        self.verticalLayout.addWidget(self.tableWidget_SymbolInfo)
-        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.tableWidget_SymbolInfo, 2, 0, 1, 1)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lineEdit_SearchInput = QtWidgets.QLineEdit(Form)
+        self.lineEdit_SearchInput.setObjectName("lineEdit_SearchInput")
+        self.horizontalLayout.addWidget(self.lineEdit_SearchInput)
+        self.pushButton_Search = QtWidgets.QPushButton(Form)
+        self.pushButton_Search.setObjectName("pushButton_Search")
+        self.horizontalLayout.addWidget(self.pushButton_Search)
+        self.pushButton_Help = QtWidgets.QPushButton(Form)
+        self.pushButton_Help.setText("")
+        self.pushButton_Help.setObjectName("pushButton_Help")
+        self.horizontalLayout.addWidget(self.pushButton_Help)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.textBrowser_AddressInfo = QtWidgets.QTextBrowser(Form)
+        self.textBrowser_AddressInfo.setObjectName("textBrowser_AddressInfo")
+        self.gridLayout.addWidget(self.textBrowser_AddressInfo, 1, 0, 1, 1)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -58,10 +54,10 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Functions"))
-        self.lineEdit_SearchInput.setPlaceholderText(_translate("Form", "Enter the regex. Leave blank to see all functions"))
-        self.pushButton_Search.setText(_translate("Form", "Search(Enter)"))
         item = self.tableWidget_SymbolInfo.horizontalHeaderItem(0)
         item.setText(_translate("Form", "Address"))
         item = self.tableWidget_SymbolInfo.horizontalHeaderItem(1)
         item.setText(_translate("Form", "Symbol"))
+        self.lineEdit_SearchInput.setPlaceholderText(_translate("Form", "Enter the regex. Leave blank to see all functions"))
+        self.pushButton_Search.setText(_translate("Form", "Search(Enter)"))
 
