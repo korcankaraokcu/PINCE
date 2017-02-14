8b09f229d9df16a145bf86598a488cebed251649
korcankaraokcu <korcankaraokcu@gmail.com>
2017-02-14 19:05:33 +0200

    Implemented SearchOpcodeWidget

--- GUI/MemoryViewerWindow.py
+++ GUI/MemoryViewerWindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MemoryViewerWindow.ui'
 #
-# Created: Sat Feb 11 01:09:25 2017
+# Created: Tue Feb 14 18:00:33 2017
 #      by: PyQt5 UI code generator 5.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -698,6 +698,8 @@ class Ui_MainWindow_MemoryView(object):
         self.actionLibPINCE.setObjectName("actionLibPINCE")
         self.actionGDB_Log_File = QtWidgets.QAction(MainWindow_MemoryView)
         self.actionGDB_Log_File.setObjectName("actionGDB_Log_File")
+        self.actionSearch_Opcode = QtWidgets.QAction(MainWindow_MemoryView)
+        self.actionSearch_Opcode.setObjectName("actionSearch_Opcode")
         self.menuView.addAction(self.actionBookmarks)
         self.menuView.addAction(self.actionStackTrace_Info)
         self.menuView.addAction(self.actionBreakpoints)
@@ -713,6 +715,7 @@ class Ui_MainWindow_MemoryView(object):
         self.menuDebug.addAction(self.actionSet_Address)
         self.menuTools.addAction(self.actionInject_so_file)
         self.menuTools.addAction(self.actionCall_Function)
+        self.menuTools.addAction(self.actionSearch_Opcode)
         self.menuFile.addAction(self.actionLoad_Trace)
         self.menuHelp.addAction(self.actionLibPINCE)
         self.menubar.addAction(self.menuFile.menuAction())
@@ -826,8 +829,9 @@ class Ui_MainWindow_MemoryView(object):
         self.actionLoad_Trace.setText(_translate("MainWindow_MemoryView", "Load Trace"))
         self.actionLibPINCE.setText(_translate("MainWindow_MemoryView", "libPINCE"))
         self.actionGDB_Log_File.setText(_translate("MainWindow_MemoryView", "GDB Log File"))
+        self.actionSearch_Opcode.setText(_translate("MainWindow_MemoryView", "Search Opcode"))
 
-from GUI.CustomTableViews.HexView import QHexView
 from GUI.CustomLabels.FlagRegisterLabel import QFlagRegisterLabel
 from GUI.CustomTableViews.AsciiView import QAsciiView
 from GUI.CustomLabels.RegisterLabel import QRegisterLabel
+from GUI.CustomTableViews.HexView import QHexView
