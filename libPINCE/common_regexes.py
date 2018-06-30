ec0ba6b6f2adb671955cfd71b7c165c9a0913a3f
korcankaraokcu <korcankaraokcu@gmail.com>
2018-06-30 18:02:51 +0300

    Possible fix for #37

--- libPINCE/common_regexes.py
+++ libPINCE/common_regexes.py
@@ -36,8 +36,12 @@ address_without_symbol = compile(hex_number_grouped.pattern + r":")  # 0x1f58010
 # * 1    Thread 0x7f34730d77c0 (LWP 6189)
 thread_info_multiple_threads = compile(r"\*\s+\d+\s+Thread\s+(" + hex_number.pattern + r"\s+\(LWP\s+\d+\))")
 # Output is like this if the inferior has only one thread
-# * 1    process 2935 [process name] 0x00007fb29406faba
+# * 1    process 2935 process name 0x00007fb29406faba
 thread_info_single_thread = compile(r"\*\s+\d+\s+process.*" + hex_number_grouped.pattern)
+# Output is like this on multithreaded wine processes, reported by user202729
+# * 1    LWP 7406 "process name" 0xf7ffade9
+thread_info_lwp_only = compile(r"\*\s+\d+\s+(LWP\s+\d+).*" + hex_number.pattern)
+thread_info_regex_list = [thread_info_multiple_threads, thread_info_single_thread, thread_info_lwp_only]
 inferior_pid = compile(r"process\s+(\d+)")
 numbers = compile(r"\d+")
 hw_breakpoint_count = compile(r"(hw|read|acc)")
