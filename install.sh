3457cbd25f693e131814a7e3acdbf50f6317cc87
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2020-04-17 21:44:43 +0300

    added scanmem as submodule
    now installs scanmem when you run install.sh, also updated some of the
    shell scripts to more modern versions of it. Made it support more OSes,
    for now it supports Arch linux as well, but NOTICE, YOU HAVE TO HAVE GCC
    AND G++ INSTALLED FOR IT TO WORK, IT WILL NOT WORK IF YOU DO NOT HAVE
    THEM INSTALLED

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '
 
+CURRENT_USER="$(who mom likes | awk '{print $1}')"
+
 install_gdb () {
 if ! sudo sh install_gdb.sh ; then
     echo "Failed to install PINCE"
@@ -23,38 +25,82 @@ if ! sudo sh install_gdb.sh ; then
 fi
 }
 
+# assumes you're in scanmem directory
+compile_scanmem() {
+    sh autogen.sh
+    ./configure --prefix=/usr 
+    make -j libscanmem.la
+    chown -R "${CURRENT_USER}":"${CURRENT_USER}" . # give permissions for normal user to change file
+}
+
+install_scanmem() {
+    if [ ! -d "./scanmem" ]; then
+        echo "Downloading scanmem"
+        git submodule update --init --recursive
+    fi
+    if [ ! -d "libPINCE/libscanmem" ]; then
+        mkdir libPINCE/libscanmem
+        chown -R "${CURRENT_USER}":"${CURRENT_USER}" libPINCE/libscanmem
+    fi
+    (
+        echo "Entering scanmem"
+        cd scanmem || exit
+        if [ -d "./.libs" ]; then 
+            echo "Recompile scanmem? [y/n]"
+            read -r answer
+            if echo "$answer" | grep -iq "^[Yy]"; then
+                compile_scanmem
+            fi
+        else
+            compile_scanmem
+        fi
+        cp --preserve .libs/libscanmem.so ../libPINCE/libscanmem/libscanmem.so
+        cp --preserve gui/scanmem.py ../libPINCE/libscanmem
+        cp --preserve gui/misc.py ../libPINCE/libscanmem
+        echo "Exitting scanmem"
+    )
+}
+
 OS_NAME="Debian"
 PKG_MGR="apt-get"
+INSTALL_COMMAND="install"
+
 PKG_NAMES_ALL="python3-setuptools python3-pip"
 PKG_NAMES_DEBIAN="$PKG_NAMES_ALL python3-pyqt5"
 PKG_NAMES_SUSE="$PKG_NAMES_ALL python3-qt5"
+PKG_NAMES_ARCH="python-pyqt5 python-setuptools" # arch defaults to py3 now days
 PKG_NAMES="$PKG_NAMES_DEBIAN"
 PKG_NAMES_PIP="psutil pexpect distorm3 pygdbmi"
 
-LSB_RELEASE="`which lsb_release`"
+LSB_RELEASE="$(command -v lsb_release)"
 if [ -n "$LSB_RELEASE" ] ; then
-    OS_NAME="`$LSB_RELEASE -d -s`"
+    OS_NAME="$(${LSB_RELEASE} -d -s)"
 fi
-
 case "$OS_NAME" in
 *SUSE*)
     PKG_MGR="zypper"
     PKG_NAMES="$PKG_NAMES_SUSE"
     ;;
+*Arch*)
+    PKG_MGR="pacman"
+    PKG_NAMES="$PKG_NAMES_ARCH"
+    INSTALL_COMMAND="-S"
+    ;;
 esac
 
-sudo $PKG_MGR install $PKG_NAMES
-sudo pip3 install $PKG_NAMES_PIP
+sudo ${PKG_MGR} ${INSTALL_COMMAND} ${PKG_NAMES}
+sudo pip3 install ${PKG_NAMES_PIP}
 
 if [ -e libPINCE/gdb_pince/gdb-8.3.1/bin/gdb ] ; then
     echo "GDB has been already compiled&installed, recompile&install? (y/n)"
-    read answer
+    read -r answer
     if echo "$answer" | grep -iq "^[Yy]" ;then
         install_gdb
     fi
 else
     install_gdb
 fi
+install_scanmem
 
 echo "PINCE has been installed successfully!"
 echo "Now, just run 'sh PINCE.sh' from terminal"
