9c32902616cd216c202b9705ec7f5276f3b01b19
Berk Özler <brkozler@hotmail.com>
2023-02-17 14:32:43 +0200

    Add Python 3 dev headers for APT distros

--- install_pince.sh
+++ install_pince.sh
@@ -106,7 +106,7 @@ ask_pkg_mgr() {
 }
 
 PKG_NAMES_ALL="python3-pip gdb libtool intltool"
-PKG_NAMES_DEBIAN="$PKG_NAMES_ALL libreadline-dev"
+PKG_NAMES_DEBIAN="$PKG_NAMES_ALL libreadline-dev python3-dev"
 PKG_NAMES_SUSE="$PKG_NAMES_ALL gcc readline-devel python3-devel typelib-1_0-Gtk-3_0 make"
 PKG_NAMES_FEDORA="$PKG_NAMES_ALL readline-devel python3-devel"
 PKG_NAMES_ARCH="python-pip readline intltool gdb lsb-release" # arch defaults to py3 nowadays
