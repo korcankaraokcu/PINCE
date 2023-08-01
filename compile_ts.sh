dcf55e6100a6beb97eae2b05930412e0e4a34432
detiam <dehe_tian@outlook.com>
2023-08-01 08:36:21 +0800

    Add zh_CN in compile_ts.sh

--- compile_ts.sh
+++ compile_ts.sh
@@ -1,1 +1,5 @@
-pylupdate6 GUI/*.ui tr/tr.py --no-obsolete --ts i18n/ts/it_IT.ts
+#!/bin/bash
+langlist="it_IT zh_CN"
+for lang in $langlist; do
+    pylupdate6 GUI/*.ui tr/tr.py --no-obsolete --ts i18n/ts/"$lang".ts
+done
