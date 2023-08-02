58a8c44fb3b14aa17bc061be5cd9d43e8010e186
detiam <dehe_tian@outlook.com>
2023-08-02 23:57:41 +0800

    Revert "If startup fails, apply venv and try again"
    This reverts commit 17c9cbd2930a036b8652b4c450decc665b0ea0b1.

--- PINCE.sh
+++ PINCE.sh
@@ -16,27 +16,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '
 
-function check_apply_venv {
-	if [ ! -d ".venv/PINCE" ]; then
-		echo "Please run \"sh install_pince.sh\" first!"
-		exit 1
-	fi
-	. .venv/PINCE/bin/activate
-}
-
-function start {
-	# Preserve env vars to keep settings like theme preferences.
-	# Debian/Ubuntu does not preserve PATH through sudo even with -E for security reasons
-	# so we need to force PATH preservation with venv activated user's PATH.
-	sudo -E --preserve-env=PATH PYTHONDONTWRITEBYTECODE=1 python3 PINCE.py
-}
-
 if [ "$(id -u)" = "0" ]; then
 	echo "Please do not run this script as root!"
 	exit 1
 fi
 
-[[ -z $USE_SYSTEM_PYTHON ]] && check_apply_venv
+if [[ -z $USE_SYSTEM_PYTHON ]]; then
+	if [ ! -d ".venv/PINCE" ]; then
+		echo "Please run \"sh install_pince.sh\" first!"
+		exit 1
+	fi
+	. .venv/PINCE/bin/activate
+fi
 
-# If exit code not 0, apply venv and try again
-start || (check_apply_venv && start)
+# Preserve env vars to keep settings like theme preferences.
+# Debian/Ubuntu does not preserve PATH through sudo even with -E for security reasons
+# so we need to force PATH preservation with venv activated user's PATH.
+sudo -E --preserve-env=PATH PYTHONDONTWRITEBYTECODE=1 python3 PINCE.py
