a1abc9c5da4c155dc22b1c2ec5aaf25cd6ad701d
Korcan Karaokçu <korcankaraokcu@gmail.com>
2023-08-02 19:34:48 +0300

    Make compile_ts POSIX compatible

--- compile_ts.sh
+++ compile_ts.sh
@@ -2,13 +2,13 @@
 file_supportlocale='/usr/share/i18n/SUPPORTED'
 list_ts=$(find i18n/ts -maxdepth 1 -type f -name '*.ts')
 
-if [[ -e $file_supportlocale ]]; then
+if [ -e "$file_supportlocale" ]; then
 	for ts in $list_ts; do
 		# Check if the locale is valid
 		if grep -E "^$(basename "$ts" .ts)(.)?.*\s.*" "$file_supportlocale" > /dev/null 2>&1; then
 			# Remove empty file to prevent error
 			# "invalid translation file: no element found: line 1, column 0"
-			[[ -s $ts ]] || rm "$ts"
+			[ -s "$ts" ] || rm "$ts"
 			pylupdate6 GUI/*.ui tr/tr.py --no-obsolete --ts "$ts"
 		else
 			list_invalidts="$list_invalidts $ts"
@@ -19,8 +19,9 @@ else
 	exit 2
 fi
 
-if [[ -n $list_invalidts ]]; then
+if [ -n "$list_invalidts" ]; then
 	echo
-	echo -e "ERROR: The following locales are invalid, please check: \n $list_invalidts"
+	echo "ERROR: The following locales are invalid, please check:"
+	echo "$list_invalidts"
 	exit 1
 fi
