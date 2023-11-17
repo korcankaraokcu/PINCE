bd692961961c8fab6f26aab2658034b69b9bd237
Korcan Karaokçu <korcankaraokcu@gmail.com>
2023-11-17 21:19:59 +0300

    Use locale_alias instead of SUPPORTED file

--- compile_ts.sh
+++ compile_ts.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-file_supportlocale='/usr/share/i18n/SUPPORTED'
+supported_locale_list=$(python3 -c "
+import locale
+print('\n'.join(value.split('.')[0] for value in locale.locale_alias.values()))
+")
 list_ts=$(find i18n/ts -maxdepth 1 -type f -name '*.ts')
 
 # If there's a user parameter, create a new locale based on it
@@ -7,20 +10,15 @@ if [ -n "$1" ]; then
     list_ts="$list_ts i18n/ts/$1.ts"
 fi
 
-if [ -e "$file_supportlocale" ]; then
-	for ts in $list_ts; do
-		# Check if the locale is valid
-		if grep -E "^$(basename "$ts" .ts)(.)?.*\s.*" "$file_supportlocale" > /dev/null 2>&1; then
-			pylupdate6 GUI/*.ui tr/tr.py --no-obsolete --ts "$ts"
-			python3 fix_ts.py "$ts"
-		else
-			list_invalidts="$list_invalidts $ts"
-		fi
-	done
-else
-	echo "file $file_supportlocale not exist, aborting"
-	exit 2
-fi
+for ts in $list_ts; do
+	# Check if the locale is valid
+	if echo "$supported_locale_list" | grep -q "$(basename "$ts" .ts)"; then
+		pylupdate6 GUI/*.ui tr/tr.py --no-obsolete --ts "$ts"
+		python3 fix_ts.py "$ts"
+	else
+		list_invalidts="$list_invalidts $ts"
+	fi
+done
 
 if [ -n "$list_invalidts" ]; then
 	echo
