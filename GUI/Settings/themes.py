c425480201fdbad11fd2b41d1fa53eb82eba2c14
Jake Rosado <jakerosado@users.noreply.github.com>
2023-12-27 13:02:40 -0500

    Refactor themes with suggested changes

--- GUI/Settings/themes.py
+++ GUI/Settings/themes.py
@@ -1,10 +1,40 @@
 from PyQt6.QtGui import QColor, QPalette
 
+theme_list = [
+    "Dark",
+    "Light",
+    "System Default",
+    "Wong (Colorblind Friendly)"
+]
 
-theme_list = ["Dark",
-              "Light",
-              "System Default",
-              "Wong (Colorblind Friendly)"]
+grp_dict = {
+    "ACTIVE": QPalette.ColorGroup.Active,
+    "INACTIVE": QPalette.ColorGroup.Inactive,
+    "DISABLED": QPalette.ColorGroup.Disabled
+}
+
+role_dict = {
+    "WINDOW_TEXT": QPalette.ColorRole.WindowText,
+    "BUTTON": QPalette.ColorRole.Button,
+    "LIGHT": QPalette.ColorRole.Light,
+    "MID_LIGHT": QPalette.ColorRole.Midlight,
+    "DARK": QPalette.ColorRole.Dark,
+    "MID": QPalette.ColorRole.Mid,
+    "TEXT": QPalette.ColorRole.Text,
+    "BRIGHT_TEXT": QPalette.ColorRole.BrightText,
+    "BUTTON_TEXT": QPalette.ColorRole.ButtonText,
+    "BASE": QPalette.ColorRole.Base,
+    "WINDOW": QPalette.ColorRole.Window,
+    "SHADOW": QPalette.ColorRole.Shadow,
+    "HIGHLIGHT": QPalette.ColorRole.Highlight,
+    "HIGHLIGHTED_TEXT": QPalette.ColorRole.HighlightedText,
+    "LINK": QPalette.ColorRole.Link,
+    "LINK_VISITED": QPalette.ColorRole.LinkVisited,
+    "ALTERNATE_BASE": QPalette.ColorRole.AlternateBase,
+    "TOOLTIP_BASE": QPalette.ColorRole.ToolTipBase,
+    "TOOLTIP_TEXT": QPalette.ColorRole.ToolTipText,
+    "PLACEHOLDER_TEXT": QPalette.ColorRole.PlaceholderText
+}
 
 
 # TODO: Previewing themes in settings window would drastically increase usability
@@ -19,99 +49,106 @@ def change_theme(new_theme):
     """
     match new_theme:
         case "Dark":
-            dup_dict = {"WINDOW_TEXT": "#FFFFFF",
-                        "BUTTON": "#241F31",
-                        "LIGHT": "#362E49",
-                        "MID_LIGHT": "#2D263D",
-                        "DARK": "#120F18",
-                        "MID": "#181521",
-                        "TEXT": "#FFFFFF",
-                        "BRIGHT_TEXT": "#FFFFFF",
-                        "BUTTON_TEXT": "#FFFFFF",
-                        "BASE": "#000000",
-                        "WINDOW": "#241F31",
-                        "SHADOW": "#000000",
-                        "HIGHLIGHT": "#308CC6",
-                        "HIGHLIGHTED_TEXT": "#FFFFFF",
-                        "LINK": "#0000FF",
-                        "LINK_VISITED": "#FF00FF",
-                        "ALTERNATE_BASE": "#120F18",
-                        "TOOLTIP_BASE": "#FFFFDC",
-                        "TOOLTIP_TEXT": "#000000",
-                        "PLACEHOLDER_TEXT": "#80FFFFFF"}
+            dup_dict = {
+                "WINDOW_TEXT": "#FFFFFF",
+                "BUTTON": "#241F31",
+                "LIGHT": "#80FFFFFF",
+                "MID_LIGHT": "#2D263D",
+                "DARK": "#80000000",
+                "MID": "#000000",
+                "TEXT": "#FFFFFF",
+                "BRIGHT_TEXT": "#FFFFFF",
+                "BUTTON_TEXT": "#FFFFFF",
+                "BASE": "#000000",
+                "WINDOW": "#241F31",
+                "SHADOW": "#000000",
+                "HIGHLIGHT": "#308CC6",
+                "HIGHLIGHTED_TEXT": "#FFFFFF",
+                "LINK": "#0000FF",
+                "LINK_VISITED": "#FF00FF",
+                "ALTERNATE_BASE": "#120F18",
+                "TOOLTIP_BASE": "#FFFFDC",
+                "TOOLTIP_TEXT": "#000000",
+                "PLACEHOLDER_TEXT": "#80FFFFFF"
+            }
 
             dark_dict = {
                 "ACTIVE": dup_dict,
                 "INACTIVE": dup_dict,
-                "DISABLED": {"WINDOW_TEXT": "#120F18",
-                             "BUTTON": "#241F31",
-                             "LIGHT": "#362E49",
-                             "MID_LIGHT": "#2D263D",
-                             "DARK": "#120F18",
-                             "MID": "#181521",
-                             "TEXT": "#120F18",
-                             "BRIGHT_TEXT": "#FFFFFF",
-                             "BUTTON_TEXT": "#120F18",
-                             "BASE": "#241F31",
-                             "WINDOW": "#241F31",
-                             "SHADOW": "#000000",
-                             "HIGHLIGHT": "#919191",
-                             "HIGHLIGHTED_TEXT": "#FFFFFF",
-                             "LINK": "#0000FF",
-                             "LINK_VISITED": "#FF00FF",
-                             "ALTERNATE_BASE": "#241F31",
-                             "TOOLTIP_BASE": "#FFFFDC",
-                             "TOOLTIP_TEXT": "#000000",
-                             "PLACEHOLDER_TEXT": "#80FFFFFF"},
+                "DISABLED": {
+                    "WINDOW_TEXT": "#80FFFFFF",
+                    "BUTTON": "#241F31",
+                    "LIGHT": "#362E49",
+                    "MID_LIGHT": "#2D263D",
+                    "DARK": "#120F18",
+                    "MID": "#181521",
+                    "TEXT": "#120F18",
+                    "BRIGHT_TEXT": "#FFFFFF",
+                    "BUTTON_TEXT": "#80FFFFFF",
+                    "BASE": "#241F31",
+                    "WINDOW": "#241F31",
+                    "SHADOW": "#000000",
+                    "HIGHLIGHT": "#919191",
+                    "HIGHLIGHTED_TEXT": "#FFFFFF",
+                    "LINK": "#0000FF",
+                    "LINK_VISITED": "#FF00FF",
+                    "ALTERNATE_BASE": "#241F31",
+                    "TOOLTIP_BASE": "#FFFFDC",
+                    "TOOLTIP_TEXT": "#000000",
+                    "PLACEHOLDER_TEXT": "#80FFFFFF"
+                },
             }
             dark_palette = update_theme(dark_dict)
             return dark_palette
         case "Light":
-            dup_dict = {"WINDOW_TEXT": "#000000",
-                        "BUTTON": "#EFEFEF",
-                        "LIGHT": "#FFFFFF",
-                        "MID_LIGHT": "#CACACA",
-                        "DARK": "#9F9F9F",
-                        "MID": "#B8B8B8",
-                        "TEXT": "#000000",
-                        "BRIGHT_TEXT": "#FFFFFF",
-                        "BUTTON_TEXT": "#000000",
-                        "BASE": "#FFFFFF",
-                        "WINDOW": "#EFEFEF",
-                        "SHADOW": "#767676",
-                        "HIGHLIGHT": "#308CC6",
-                        "HIGHLIGHTED_TEXT": "#FFFFFF",
-                        "LINK": "#0000FF",
-                        "LINK_VISITED": "#FF00FF",
-                        "ALTERNATE_BASE": "#F7F7F7",
-                        "TOOLTIP_BASE": "#FFFFDC",
-                        "TOOLTIP_TEXT": "#000000",
-                        "PLACEHOLDER_TEXT": "#80000000"
-                        }
+            dup_dict = {
+                "WINDOW_TEXT": "#000000",
+                "BUTTON": "#EFEFEF",
+                "LIGHT": "#FFFFFF",
+                "MID_LIGHT": "#CACACA",
+                "DARK": "#5E5C64",
+                "MID": "#B8B8B8",
+                "TEXT": "#000000",
+                "BRIGHT_TEXT": "#FFFFFF",
+                "BUTTON_TEXT": "#000000",
+                "BASE": "#FFFFFF",
+                "WINDOW": "#EFEFEF",
+                "SHADOW": "#767676",
+                "HIGHLIGHT": "#308CC6",
+                "HIGHLIGHTED_TEXT": "#FFFFFF",
+                "LINK": "#0000FF",
+                "LINK_VISITED": "#FF00FF",
+                "ALTERNATE_BASE": "#F7F7F7",
+                "TOOLTIP_BASE": "#FFFFDC",
+                "TOOLTIP_TEXT": "#000000",
+                "PLACEHOLDER_TEXT": "#80000000"
+            }
 
             light_dict = {
                 "ACTIVE": dup_dict,
                 "INACTIVE": dup_dict,
-                "DISABLED": {"WINDOW_TEXT": "#BEBEBE",
-                             "BUTTON": "#EFEFEF",
-                             "LIGHT": "#FFFFFF",
-                             "MID_LIGHT": "#CACACA",
-                             "DARK": "#BEBEBE",
-                             "MID": "#B8B8B8",
-                             "TEXT": "#BEBEBE",
-                             "BRIGHT_TEXT": "#FFFFFF",
-                             "BUTTON_TEXT": "#BEBEBE",
-                             "BASE": "#EFEFEF",
-                             "WINDOW": "#EFEFEF",
-                             "SHADOW": "#B1B1B1",
-                             "HIGHLIGHT": "#919191",
-                             "HIGHLIGHTED_TEXT": "#FFFFFF",
-                             "LINK": "#0000FF",
-                             "LINK_VISITED": "#FF00FF",
-                             "ALTERNATE_BASE": "#F7F7F7",
-                             "TOOLTIP_BASE": "#FFFFDC",
-                             "TOOLTIP_TEXT": "#000000",
-                             "PLACEHOLDER_TEXT": "#80000000"},
+                "DISABLED": {
+                    "WINDOW_TEXT": "#BEBEBE",
+                    "BUTTON": "#EFEFEF",
+                    "LIGHT": "#FFFFFF",
+                    "MID_LIGHT": "#CACACA",
+                    "DARK": "#BEBEBE",
+                    "MID": "#B8B8B8",
+                    "TEXT": "#BEBEBE",
+                    "BRIGHT_TEXT": "#FFFFFF",
+                    "BUTTON_TEXT": "#BEBEBE",
+                    "BASE": "#EFEFEF",
+                    "WINDOW": "#EFEFEF",
+                    "SHADOW": "#B1B1B1",
+                    "HIGHLIGHT": "#919191",
+                    "HIGHLIGHTED_TEXT": "#FFFFFF",
+                    "LINK": "#0000FF",
+                    "LINK_VISITED": "#FF00FF",
+                    "ALTERNATE_BASE": "#F7F7F7",
+                    "TOOLTIP_BASE": "#FFFFDC",
+                    "TOOLTIP_TEXT": "#000000",
+                    "PLACEHOLDER_TEXT": "#80000000"
+                },
             }
             light_palette = update_theme(light_dict)
             return light_palette
@@ -119,51 +156,54 @@ def change_theme(new_theme):
             sys_default = QPalette()
             return sys_default
         case "Wong (Colorblind Friendly)":
-            dup_dict = {"WINDOW_TEXT": "#000000",
-                        "BUTTON": "#E69F00",
-                        "LIGHT": "#000000",
-                        "MID_LIGHT": "#000000",
-                        "DARK": "#000000",
-                        "MID": "#000000",
-                        "TEXT": "#000000",
-                        "BRIGHT_TEXT": "#FFFFFF",
-                        "BUTTON_TEXT": "#000000",
-                        "BASE": "#E69F00",
-                        "WINDOW": "#F0E442",
-                        "SHADOW": "#009E73",
-                        "HIGHLIGHT": "#0072B2",
-                        "HIGHLIGHTED_TEXT": "#FFFFFF",
-                        "LINK": "#56B4E9",
-                        "LINK_VISITED": "#CC79A7",
-                        "ALTERNATE_BASE": "#E69F00",
-                        "TOOLTIP_BASE": "#FFFFDC",
-                        "TOOLTIP_TEXT": "#000000",
-                        "PLACEHOLDER_TEXT": "#80000000"
-                        }
+            dup_dict = {
+                "WINDOW_TEXT": "#000000",
+                "BUTTON": "#E69F00",
+                "LIGHT": "#FFFFFF",
+                "MID_LIGHT": "#000000",
+                "DARK": "#000000",
+                "MID": "#000000",
+                "TEXT": "#000000",
+                "BRIGHT_TEXT": "#FFFFFF",
+                "BUTTON_TEXT": "#000000",
+                "BASE": "#E69F00",
+                "WINDOW": "#009E73",
+                "SHADOW": "#009E73",
+                "HIGHLIGHT": "#0072B2",
+                "HIGHLIGHTED_TEXT": "#FFFFFF",
+                "LINK": "#56B4E9",
+                "LINK_VISITED": "#CC79A7",
+                "ALTERNATE_BASE": "#E69F00",
+                "TOOLTIP_BASE": "#FFFFDC",
+                "TOOLTIP_TEXT": "#000000",
+                "PLACEHOLDER_TEXT": "#80000000"
+            }
 
             wong_dict = {
                 "ACTIVE": dup_dict,
                 "INACTIVE": dup_dict,
-                "DISABLED": {"WINDOW_TEXT": "#D55E00",
-                             "BUTTON": "#D55E00",
-                             "LIGHT": "#FFFFFF",
-                             "MID_LIGHT": "#FFFFFF",
-                             "DARK": "#FFFFFF",
-                             "MID": "#FFFFFF",
-                             "TEXT": "#FFFFFF",
-                             "BRIGHT_TEXT": "#000000",
-                             "BUTTON_TEXT": "#D55E00",
-                             "BASE": "#D55E00",
-                             "WINDOW": "#000000",
-                             "SHADOW": "#F0E442",
-                             "HIGHLIGHT": "#919191",
-                             "HIGHLIGHTED_TEXT": "#000000",
-                             "LINK": "#56b4E9",
-                             "LINK_VISITED": "#CC79A7",
-                             "ALTERNATE_BASE": "#919191",
-                             "TOOLTIP_BASE": "#000000",
-                             "TOOLTIP_TEXT": "#FFFFFF",
-                             "PLACEHOLDER_TEXT": "#80000000"},
+                "DISABLED": {
+                    "WINDOW_TEXT": "#80000000",
+                    "BUTTON": "#E69F00",
+                    "LIGHT": "#FFFFFF",
+                    "MID_LIGHT": "#FFFFFF",
+                    "DARK": "#FFFFFF",
+                    "MID": "#FFFFFF",
+                    "TEXT": "#FFFFFF",
+                    "BRIGHT_TEXT": "#000000",
+                    "BUTTON_TEXT": "#80000000",
+                    "BASE": "#E69F00",
+                    "WINDOW": "#000000",
+                    "SHADOW": "#F0E442",
+                    "HIGHLIGHT": "#919191",
+                    "HIGHLIGHTED_TEXT": "#000000",
+                    "LINK": "#56b4E9",
+                    "LINK_VISITED": "#CC79A7",
+                    "ALTERNATE_BASE": "#919191",
+                    "TOOLTIP_BASE": "#000000",
+                    "TOOLTIP_TEXT": "#FFFFFF",
+                    "PLACEHOLDER_TEXT": "#80000000"
+                },
             }
             wong_palette = update_theme(wong_dict)
             return wong_palette
@@ -181,65 +221,12 @@ def update_theme(cur_dict):
         QPalette: Self-explanatory
     """
     new_palette = QPalette()
-    cur_grp = None
-    cur_role = None
 
     for group in cur_dict:
-        match group:
-            case "ACTIVE":
-                cur_grp = QPalette.ColorGroup.Active
-            case "INACTIVE":
-                cur_grp = QPalette.ColorGroup.Inactive
-            case "DISABLED":
-                cur_grp = QPalette.ColorGroup.Disabled
-            case _:
-                print("There was an error parsing theme groups.")
+        cur_grp = grp_dict[group]
 
         for color in cur_dict[group]:
-            match color:
-                case "WINDOW_TEXT":
-                    cur_role = QPalette.ColorRole.WindowText
-                case "BUTTON":
-                    cur_role = QPalette.ColorRole.Button
-                case "LIGHT":
-                    cur_role = QPalette.ColorRole.Light
-                case "MID_LIGHT":
-                    cur_role = QPalette.ColorRole.Midlight
-                case "DARK":
-                    cur_role = QPalette.ColorRole.Dark
-                case "MID":
-                    cur_role = QPalette.ColorRole.Mid
-                case "TEXT":
-                    cur_role = QPalette.ColorRole.Text
-                case "BRIGHT_TEXT":
-                    cur_role = QPalette.ColorRole.BrightText
-                case "BUTTON_TEXT":
-                    cur_role = QPalette.ColorRole.ButtonText
-                case "BASE":
-                    cur_role = QPalette.ColorRole.Base
-                case "WINDOW":
-                    cur_role = QPalette.ColorRole.Window
-                case "SHADOW":
-                    cur_role = QPalette.ColorRole.Shadow
-                case "HIGHLIGHT":
-                    cur_role = QPalette.ColorRole.Highlight
-                case "HIGHLIGHTED_TEXT":
-                    cur_role = QPalette.ColorRole.HighlightedText
-                case "LINK":
-                    cur_role = QPalette.ColorRole.Link
-                case "LINK_VISITED":
-                    cur_role = QPalette.ColorRole.LinkVisited
-                case "ALTERNATE_BASE":
-                    cur_role = QPalette.ColorRole.AlternateBase
-                case "TOOLTIP_BASE":
-                    cur_role = QPalette.ColorRole.ToolTipBase
-                case "TOOLTIP_TEXT":
-                    cur_role = QPalette.ColorRole.ToolTipText
-                case "PLACEHOLDER_TEXT":
-                    cur_role = QPalette.ColorRole.PlaceholderText
-                case _:
-                    print("There was an error parsing theme colors.")
-
+            cur_role = role_dict[color]
             new_palette.setColor(cur_grp, cur_role, QColor(cur_dict[group][color]))
 
     return new_palette
