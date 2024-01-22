8af10384f1fbd2b0ea7056b5863cd6e8a910b5e3
Korcan Karaokçu <korcankaraokcu@gmail.com>
2024-01-22 15:46:25 +0300

    Use QStyledItemDelegate and update type hints

--- GUI/ItemDelegates/HexDelegate.py
+++ GUI/ItemDelegates/HexDelegate.py
@@ -14,18 +14,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from PyQt6.QtWidgets import QItemDelegate, QLineEdit
+from PyQt6.QtWidgets import QStyledItemDelegate, QLineEdit, QWidget
 from PyQt6.QtGui import QRegularExpressionValidator
-from PyQt6.QtCore import Qt, QRegularExpression
+from PyQt6.QtCore import QModelIndex, Qt, QRegularExpression
 
 
-class QHexDelegate(QItemDelegate):
-    def __init__(self, max_length=2, regexp="[0-9a-fA-F]+", parent=None):
+class QHexDelegate(QStyledItemDelegate):
+    def __init__(self, max_length: int = 2, regexp: str = "[0-9a-fA-F]+", parent=None) -> None:
         super().__init__(parent)
         self.max_length = max_length
         self.regexp = regexp
 
-    def createEditor(self, parent, option, index):
+    def createEditor(self, parent: QWidget, option, index: QModelIndex) -> QLineEdit:
         self.editor = QLineEdit(parent)
         self.editor.setMaxLength(self.max_length)
         hex_validator = QRegularExpressionValidator(QRegularExpression(self.regexp), self.editor)
@@ -34,10 +34,10 @@ class QHexDelegate(QItemDelegate):
         self.editor.textChanged.connect(self.check_text)
         return self.editor
 
-    def setEditorData(self, editor, index):
+    def setEditorData(self, editor, index) -> None:
         # Initial text was set in createEditor, this is a trick to dodge the textChanged signal
         return
 
-    def check_text(self):
+    def check_text(self) -> None:
         if len(self.editor.text()) >= self.max_length:
-            self.closeEditor.emit(self.editor, QItemDelegate.EndEditHint.EditNextItem)
+            self.closeEditor.emit(self.editor, QStyledItemDelegate.EndEditHint.EditNextItem)
