b34f358a739dbe3593b02931364a1c1685b601de
Korcan Karaokçu <korcankaraokcu@gmail.com>
2024-02-13 17:11:35 +0300

    Bugfixes and optimizations for settings

--- GUI/Settings/settings.py
+++ GUI/Settings/settings.py
@@ -1,12 +1,13 @@
 import libpince.typedefs as typedefs
 
-current_settings_version = "32"  # Increase version by one if you change settings
+current_settings_version = "33"  # Increase version by one if you change settings
 
 # Unused, will be re-added in the future
 code_injection_method: int = 0
 
 gdb_path: str = typedefs.PATHS.GDB
 gdb_output_mode: tuple = (True, True, True)
+locale: str = "en_US"
 gdb_logging: bool = False
 interrupt_signal: str
 handle_signals: list = []
@@ -25,27 +26,7 @@ default_signals = [
     ["SIGMSG", True, True], ["SIGSOUND", True, True], ["SIGSAK", True, True], ["SIGPRIO", False, True],
     ["SIGCANCEL", False, True], ["SIGINFO", True, True], ["EXC_BAD_ACCESS", True, True],
     ["EXC_BAD_INSTRUCTION", True, True], ["EXC_ARITHMETIC", True, True], ["EXC_EMULATION", True, True],
-    ["EXC_SOFTWARE", True, True], ["EXC_BREAKPOINT", True, True], ["SIGLIBRT", False, True],
-    #
-    ['SIG32', True, True],
-    ['SIG33', True, True], ['SIG34', True, True], ['SIG35', True, True], ['SIG36', True, True], ['SIG37', True, True],
-    ['SIG38', True, True], ['SIG39', True, True], ['SIG40', True, True], ['SIG41', True, True], ['SIG42', True, True],
-    ['SIG43', True, True], ['SIG44', True, True], ['SIG45', True, True], ['SIG46', True, True], ['SIG47', True, True],
-    ['SIG48', True, True], ['SIG49', True, True], ['SIG50', True, True], ['SIG51', True, True], ['SIG52', True, True],
-    ['SIG53', True, True], ['SIG54', True, True], ['SIG55', True, True], ['SIG56', True, True], ['SIG57', True, True],
-    ['SIG58', True, True], ['SIG59', True, True], ['SIG60', True, True], ['SIG61', True, True], ['SIG62', True, True],
-    ['SIG63', True, True], ['SIG64', True, True], ['SIG65', True, True], ['SIG66', True, True], ['SIG67', True, True],
-    ['SIG68', True, True], ['SIG69', True, True], ['SIG70', True, True], ['SIG71', True, True], ['SIG72', True, True],
-    ['SIG73', True, True], ['SIG74', True, True], ['SIG75', True, True], ['SIG76', True, True], ['SIG77', True, True],
-    ['SIG78', True, True], ['SIG79', True, True], ['SIG80', True, True], ['SIG81', True, True], ['SIG82', True, True],
-    ['SIG83', True, True], ['SIG84', True, True], ['SIG85', True, True], ['SIG86', True, True], ['SIG87', True, True],
-    ['SIG88', True, True], ['SIG89', True, True], ['SIG90', True, True], ['SIG91', True, True], ['SIG92', True, True],
-    ['SIG93', True, True], ['SIG94', True, True], ['SIG95', True, True], ['SIG96', True, True], ['SIG97', True, True],
-    ['SIG98', True, True], ['SIG99', True, True], ['SIG100', True, True], ['SIG101', True, True],
-    ['SIG102', True, True], ['SIG103', True, True], ['SIG104', True, True], ['SIG105', True, True],
-    ['SIG106', True, True], ['SIG107', True, True], ['SIG108', True, True], ['SIG109', True, True],
-    ['SIG110', True, True], ['SIG111', True, True], ['SIG112', True, True], ['SIG113', True, True],
-    ['SIG114', True, True], ['SIG115', True, True], ['SIG116', True, True], ['SIG117', True, True],
-    ['SIG118', True, True], ['SIG119', True, True], ['SIG120', True, True], ['SIG121', True, True],
-    ['SIG122', True, True], ['SIG123', True, True], ['SIG124', True, True], ['SIG125', True, True],
-    ['SIG126', True, True], ['SIG127', True, True]]
+    ["EXC_SOFTWARE", True, True], ["EXC_BREAKPOINT", True, True], ["SIGLIBRT", False, True]
+]
+for x in range(32, 128):  # Add signals SIG32-SIG127
+  default_signals.append([f"SIG{x}", True, True])
