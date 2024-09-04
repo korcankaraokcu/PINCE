b72ff365a0858b4899092fd46bbfa20cbca59a97
Korcan Karaokçu <korcankaraokcu@gmail.com>
2024-09-05 00:17:38 +0300

    Remove redundant html text

--- GUI/ConsoleWidget.py
+++ GUI/ConsoleWidget.py
@@ -58,11 +58,6 @@ class Ui_Form(object):
         font.setBold(False)
         font.setItalic(False)
         self.textBrowser.setFont(font)
-        self.textBrowser.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Monospace\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>")
         self.textBrowser.setObjectName("textBrowser")
         self.verticalLayout.addWidget(self.textBrowser)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
